26100d27174c80708926889ae3fe5f1742f63996
Pragati Gupta <pragatigupta0104@gmail.com>
2024-11-21 23:30:38 +0530

    2d

--- TWO_D_ARRAY/transpose2_array.cpp
+++ TWO_D_ARRAY/transpose2_array.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-using namespcae std;
+using namespace std;
 int main()
 {
 int x,y;
@@ -8,5 +8,36 @@ cin>>x;
 cout<<"enter size of column:"<<"\n";
 cin>>y;
 int arr[x][y];
-cout<<"enter"<<x*y" 6 values:"<<"\t";
+cout<<"enter"<<x*y<<"  values\n";
+for(int r=0;r<x;r++)
+{
+
+    for(int c=0;c<y;c++)
+    {
+        cin>>arr[r][c];
+    }
+}
+for(int r=0;r<x;r++)
+{
+    for(int c=0;c<y;c++)
+    {
+        cout<<arr[r][c]<<"t";
+    }
+    cout<<"\n";
+}
+if(x==y)
+{
+    cout<<"\nAfter applying transpose concept\n";
+    for(int c=0;c<x;c++)
+    {
+        for(int r=0;r<y;r++)
+        {
+            cout<<arr[r][c]<<"\t";
+        }
+        cout<<"\n";
+    }
+}
+else{
+    cout<<"Not allowed, bcz row and column sizes are not match";
+}
 }
